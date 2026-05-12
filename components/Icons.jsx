05642e5274a5ff55ad142e5878602a6115a942c8
jaafarxalrabbat-eng <jaafar.x.alrabbat@gmail.com>
2026-05-13 01:28:42 +0200

    Create Icons.jsx

---
+++ components/Icons.jsx
@@ -0,0 +1,17 @@
+export function ArrowIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M7 17L17 7" />
+      <path d="M9 7h8v8" />
+    </svg>
+  );
+}
+
+export function PlusIcon({ open }) {
+  return (
+    <span className={open ? "faq-plus is-open" : "faq-plus"} aria-hidden="true">
+      <span />
+      <span />
+    </span>
+  );
+}
