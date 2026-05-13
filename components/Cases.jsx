f0744456a32e9854da8127b9053957fa60b605a2
jaafarxalrabbat-eng <jaafar.x.alrabbat@gmail.com>
2026-05-13 20:45:41 +0200

    Update Cases.jsx

--- components/Cases.jsx
+++ components/Cases.jsx
@@ -1,7 +1,7 @@
 import { cases } from "../content";
 import { ArrowIcon } from "./Icons";
 
-export default function Cases({ t }) {
+export default function Cases({ t, isArabic }) {
   return (
     <section id="work" className="cases-section">
       <div className="section-head">
@@ -12,6 +12,9 @@ export default function Cases({ t }) {
       <div className="cases-grid">
         {cases.map((item) => {
           const CardTag = item.link ? "a" : "article";
+          const title = isArabic ? item.titleAr || item.title : item.title;
+          const italic = isArabic ? item.italicAr || item.italic : item.italic;
+          const tags = isArabic ? item.tagsAr || item.tags : item.tags;
 
           return (
             <CardTag
@@ -21,10 +24,10 @@ export default function Cases({ t }) {
               target={item.link ? "_blank" : undefined}
               rel={item.link ? "noreferrer" : undefined}
             >
-              <img src={item.image} alt={item.title} />
+              <img src={item.image} alt={title} />
 
               <div className="case-tags">
-                {item.tags.map((tag) => (
+                {tags.map((tag) => (
                   <span key={tag}>{tag}</span>
                 ))}
               </div>
@@ -34,7 +37,7 @@ export default function Cases({ t }) {
               </span>
 
               <h3>
-                {item.title} <em>{item.italic}</em>
+                {title} <em>{italic}</em>
               </h3>
             </CardTag>
           );
